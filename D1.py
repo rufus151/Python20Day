b3c96537c8a95adf0af63603bdf9013e482cf457
rufus151 <168814950+rufus151@users.noreply.github.com>
2024-12-10 23:11:20 +0530

    Add files via upload

---
+++ D1.py
@@ -0,0 +1,38 @@
+# This is a comment
+
+''' 
+This is a 
+multi line comment
+
+'''
+
+# print ()
+print("Hello World")
+
+
+#Finding area of a rectangle
+length=10
+breadth=12
+area=length*breadth
+print(f"The area of square is {area}")
+
+#Calculating Average of three numbers
+
+n1=5
+n2=6
+n3=7
+avg=(n1+n2+n3)/3
+print(f"Average of three numbers is {avg}")
+
+
+#Asking a user for their age and Displaying it
+
+age=int(input("Enter your Age: "))
+print("Your age is:", age)
+
+#Printing a Pattern
+print("  *  ")
+print(" *** ")
+print("*****")
+print(" *** ")
+print("  *  ")
